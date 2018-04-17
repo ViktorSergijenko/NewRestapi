bb63bdaaa2fc026596faa2ffa0c45f8342d2110a
Viktor Sergijenko <viktor.sergijenko@bbit.tech>
2018-04-18 00:21:04 +0300

    Created a house component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HouseComponent } from './house/house.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HouseComponent
   ],
   imports: [
     BrowserModule
