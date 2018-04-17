b936cbb5cad2e4f27422a9787fd2cbe6c56cbaaf
Viktor Sergijenko <viktor.sergijenko@bbit.tech>
2018-04-18 00:33:48 +0300

    created house and house-list components in houses folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HouseComponent } from './house/house.component';
+import { HouseLisComponent } from './houses/house-lis/house-lis.component';
+import { HouseListComponent } from './houses/house-list/house-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HouseComponent
+    HouseComponent,
+    HouseLisComponent,
+    HouseListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/houses/house-list/house-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-house-list',
+  templateUrl: './house-list.component.html',
+  styleUrls: ['./house-list.component.css']
+})
+export class HouseListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
