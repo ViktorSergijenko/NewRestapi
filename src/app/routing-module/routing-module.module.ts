7ea437475a9394ecc4ac47dc4e3291fc9499a8dc
ViktorSergijenko <viktor.sergijenko@bbit.tech>
2018-05-18 19:29:07 -0700

    added a rout.module to the project

---
+++ src/app/routing-module/routing-module.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RoutingModuleRoutingModule } from './routing-module-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RoutingModuleRoutingModule
+  ],
+  declarations: []
+})
+export class RoutingModuleModule { }
